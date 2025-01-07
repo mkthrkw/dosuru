5366f83e5b163ac53d356f90ccb81b2ee9d25110
mkthrkw <mkt1536@gmail.com>
2025-01-08 01:05:05 +0900

    revert: prismaClientを本番環境のグローバルオブジェクト格納を取りやめ revert: 環境変数名の変更取りやめ

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -5,9 +5,7 @@ const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : [],
+    log: ["query", "info", "warn", "error"],
   });
 
-if (!globalForPrisma.prisma) {
-  globalForPrisma.prisma = prisma;
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
