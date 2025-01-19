5ec57c29be47189d084483be418d8d981c823ab9
mkthrkw <mkt1536@gmail.com>
2025-01-19 23:25:42 +0900

    Update: チケット用ファイルを修正 - チケットの各Hooksをstore利用に切り替え - チケットカードのuseContextを削除し、storeを利用へ切り替え - lintエラーを修正

--- app/_features/tickets/hooks/useTicketCompleteHandler.ts
+++ app/_features/tickets/hooks/useTicketCompleteHandler.ts
@@ -1,42 +1,44 @@
+import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
 import { toast } from "react-toastify";
+import { useTicketModalStore } from "../../lists/store/useTicketModalStore";
 import { updateTicketCompleted } from "../actions";
-import { TicketComment } from "@/app/_util/types/nestedType";
 
-export function useTicketCompleteHandler({
-  modalProps,
-  stateUpdateFunction,
-}: {
-  modalProps: TicketComment | null;
-  stateUpdateFunction?: (args: { [key: string]: boolean }) => void;
-}) {
-  const [completed, setCompleted] = useState(modalProps?.completed ?? false);
+export function useTicketCompleteHandler() {
+	const { ticketModalProps, partialUpdateTicketModalProps } =
+		useTicketModalStore();
 
-  const [isSubmitting, setIsSubmitting] = useState(false);
+	const [completed, setCompleted] = useState(
+		ticketModalProps?.completed ?? false,
+	);
+	const [isSubmitting, setIsSubmitting] = useState(false);
+	const router = useRouter();
 
-  const handleToggle = useCallback(
-    async (event: React.ChangeEvent<HTMLInputElement>) => {
-      const message = `このチケットを${completed ? "未完了" : "完了"}しますか？`;
-      if (!modalProps || !window.confirm(message)) {
-        event?.preventDefault();
-        return;
-      }
-      setIsSubmitting(true);
-      const result = await updateTicketCompleted(!completed, modalProps.id);
-      if (result.state === "resolved") {
-        setCompleted(!completed);
-        if (stateUpdateFunction) {
-          stateUpdateFunction({ completed: !completed });
-        }
-      } else if (result.state === "rejected") {
-        toast.error(result.message || "An error occurred", {
-          autoClose: 3000,
-        });
-      }
-      setIsSubmitting(false);
-    },
-    [completed, modalProps, stateUpdateFunction],
-  );
+	const handleToggle = useCallback(
+		async (event: React.ChangeEvent<HTMLInputElement>) => {
+			const message = `このチケットを${completed ? "未完了" : "完了"}しますか？`;
+			if (!ticketModalProps || !window.confirm(message)) {
+				event?.preventDefault();
+				return;
+			}
+			setIsSubmitting(true);
+			const result = await updateTicketCompleted(
+				!completed,
+				ticketModalProps.id,
+			);
+			if (result.state === "resolved") {
+				setCompleted(!completed);
+				partialUpdateTicketModalProps({ completed: !completed });
+				router.refresh();
+			} else if (result.state === "rejected") {
+				toast.error(result.message || "An error occurred", {
+					autoClose: 3000,
+				});
+			}
+			setIsSubmitting(false);
+		},
+		[completed, ticketModalProps, partialUpdateTicketModalProps, router],
+	);
 
-  return { completed, setCompleted, handleToggle, isSubmitting };
+	return { completed, setCompleted, handleToggle, isSubmitting };
 }
